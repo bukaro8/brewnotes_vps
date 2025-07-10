13a43b8fc207ba4826a48295ff3ed16205f80921
Victor Ramirez <vicdeveloper83@gmail.com>
2025-07-10 20:40:00 +0100

    host changed

--- brewnotes/settings.py
+++ brewnotes/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-%8)%9u*o2jq9k!t166g$2e*v(c6b_dw2_#u&t##dal-gu(puty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", ]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "*"]
 
 
 # Application definition
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes'
 ]
 
 MIDDLEWARE = [
@@ -46,7 +47,7 @@ ROOT_URLCONF = 'brewnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
