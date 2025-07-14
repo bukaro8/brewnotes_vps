dd5f9bd488f95897fb9cacc206f30daa9493e737
Victor Ramirez <vicdeveloper83@gmail.com>
2025-07-14 14:24:26 +0100

    antoher try

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ WORKDIR /code
 COPY requirements.txt /code/
 
 RUN python -m pip install --upgrade pip
+RUN apt-get update && apt-get install -y postgresql-client
 RUN python -m pip install -r requirements.txt
 
 COPY . /code/
--- brewnotes/settings.py
+++ brewnotes/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%8)%9u*o2jq9k!t166g$2e*v(c6b_dw2_#u&t##dal-gu(puty'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["0.0.0.0", "localhost", "*"]
 
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
