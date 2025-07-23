6e1b7ed0a51c7ed4673178da3e337e79e0365157
Victor Ramirez <vicdeveloper83@gmail.com>
2025-07-23 15:46:58 +0100

    drinktypes created

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import DrinkType
 # Register your models here.
+
+
+@admin.register(DrinkType)
+class DrinkTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DrinkType(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        help_text="The name of the drink type (e.g., IPA, Stout, Lager)")
+    description = models.TextField(
+        blank=True,
+        help_text="Detailed description of the drink type"
+    )
+
+    def __str__(self):
+        return self.name
